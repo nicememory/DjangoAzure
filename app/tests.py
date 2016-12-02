272c56463d6a993b62b708af499c681a16e33fbf
nicememory <nicememory@gmail.com>
2016-12-02 18:14:35 +0800

    Features are ready.

--- app/tests.py
+++ app/tests.py
@@ -21,12 +21,7 @@ class ViewTest(TestCase):
     def test_home(self):
         """Tests the home page."""
         response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
-
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+        self.assertContains(response, 'SF Movie Map', 2, 200)
 
     def test_about(self):
         """Tests the about page."""
